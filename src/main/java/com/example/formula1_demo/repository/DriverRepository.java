df37ea37eac2b2c4c4beb90c67a417e956e6b025
Marco Ames <marcoames5@gmail.com>
2025-02-26 18:13:19 -0300

    refactor: remove unused import in DriverRepository

--- src/main/java/com/example/formula1_demo/repository/DriverRepository.java
+++ src/main/java/com/example/formula1_demo/repository/DriverRepository.java
@@ -2,7 +2,6 @@ package com.example.formula1_demo.repository;
 
 import com.example.formula1_demo.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
