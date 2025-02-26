39013568de78a20320c370a6bbfac2e3bb6097c8
Marco Ames <marcoames5@gmail.com>
2025-02-26 11:46:22 -0300

    feat: initial commit

---
+++ src/main/java/com/example/formula1_demo/Formula1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.formula1_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formula1_demo/Formula1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formula1_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
