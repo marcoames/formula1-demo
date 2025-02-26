5221b0a4d1e8028ea4188dd6f97f85ba46f404df
Marco Ames <marcoames5@gmail.com>
2025-02-26 15:14:25 -0300

    feat: add DriverRepository interface

---
+++ src/main/java/com/example/formula1_demo/repository/DriverRepository.java
@@ -0,0 +1,11 @@
+package com.example.formula1_demo.repository;
+
+import com.example.formula1_demo.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+
+    List<Driver> findByNationality(String nationality);
+
+}
