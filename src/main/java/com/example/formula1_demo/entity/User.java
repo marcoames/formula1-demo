d315160b6ad0faa2f64927f873949d6c1bbc65db
Marco Ames <marcoames5@gmail.com>
2025-02-27 20:15:12 -0300

    feat: add User entity to represent users in the application

---
+++ src/main/java/com/example/formula1_demo/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.formula1_demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private Role role;
+
+}
